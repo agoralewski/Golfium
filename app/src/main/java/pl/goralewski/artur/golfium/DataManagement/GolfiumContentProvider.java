d28b9b52e0f222a224e85a5f57734e479e71323d
agoralewski <1arturro1@gmail.com>
2015-05-16 14:42:28 +0200

    remove???

--- app/src/main/java/pl/goralewski/artur/golfium/DataManagement/GolfiumContentProvider.java
+++
@@ -1,7 +0,0 @@
-package pl.goralewski.artur.golfium.dataManagement;
-
-/**
- * Created by Artur Góralewski on 11/05/2015.
- */
-public class GolfiumContentProvider {
-}
--- app/src/main/java/pl/goralewski/artur/golfium/DataManagement/tables/HoleTable.java
+++
@@ -1,7 +0,0 @@
-package pl.goralewski.artur.golfium.dataManagement.tables;
-
-/**
- * Created by Artur Góralewski on 14/05/2015.
- */
-public class HoleTable {
-}
